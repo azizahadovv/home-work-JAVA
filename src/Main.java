6c0e7c0c18c854ec6c86fc706be1e59c8dadd5d3
Aziz Ahadov <aziz.ahadovv@yandex.ru>
2025-06-10 15:48:51 +0500

    add

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,231 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner scannerInt = new Scanner(System.in);
+        double pi = 3.14;
+//      6
+//        System.out.print("B son kiriting> ");
+//        int a=scannerInt.nextInt();
+//        System.out.print("A son kiriting> ");
+//        int b=scannerInt.nextInt();
+//        System.out.print("C son kiriting> ");
+//        int c=scannerInt.nextInt();
+//        System.out.println("V="+a*b*c+" "+ "S="+2*(a*b+b*c+a*c));
+
+//      7
+//        System.out.print("R son kiriting> ");
+//        int r = scannerInt.nextInt();
+//        System.out.println("L=" + 2 * 3.14 * r + " " + "S=" +3.14 * r * r);
+
+//      8
+
+//        System.out.print("B son kiriting> ");
+//        int a=scannerInt.nextInt();
+//        System.out.print("A son kiriting> ");
+//        int b=scannerInt.nextInt();
+//
+//        System.out.println("o'rta arifmetik ="  + (a+b)/2 );
+
+//        9
+//        System.out.print("A son kiriting> ");
+//        int a=scannerInt.nextInt();
+//        System.out.print("B son kiriting> ");
+//        int b=scannerInt.nextInt();
+//        System.out.println("o'rta Gemoetrik ="  + Math.sqrt(a*b));
+
+//        10
+//        System.out.print("A son kiriting> ");
+//        int a = scannerInt.nextInt();
+//        System.out.print("B son kiriting> ");
+//        int b = scannerInt.nextInt();
+//        System.out.println("Yig'indisi =" + a + b + " ko'paytmasi=" + a * b + "Kvadratlari= " + a * a + " " + b * b);
+//         11
+//        System.out.print("A son kiriting> ");
+//        int a = scannerInt.nextInt();
+//        System.out.print("B son kiriting> ");
+//        int b = scannerInt.nextInt();
+//        System.out.println("Yig'indisi =" + a + b + " ko'paytmasi=" + a * b + "moduli= " + Math.abs(a) + " " + Math.abs(b));
+
+//        12
+//        System.out.print("A son kiriting> ");
+//        int a = scannerInt.nextInt();
+//        System.out.print("B son kiriting> ");
+//        int b = scannerInt.nextInt();
+//        int C= Math.sqrt(a*a+b*b) "
+
+//        System.out.println("C=" +C + " P=" + a + b + C);
+//
+//        13
+//        System.out.print("R1 son kiriting> ");
+//        int r1 = scannerInt.nextInt();
+//        System.out.print("R2 son kiriting> ");
+//        int r2 = scannerInt.nextInt();
+//        if (r1<r2) System.out.println("R1>R2 Shart qanoatlantirilsin") ;
+//        else {
+//            double S1=3.14*r1;
+//            double S2=3.14*r2;
+//            double S3=3.14*(r1-r2);
+//
+//            System.out.println("C1=" + S1 + " S2=" + S2 + " S3=" + S3);
+//        }
+
+//        14
+
+//        System.out.print("L son kiriting> ");
+//        int l = scannerInt.nextInt();
+//        double R = l / (2 * pi);
+//        double S = pi * R*R;
+//        System.out.println("R=" + R + " S=" + S );
+
+//        15
+
+//        System.out.print("L son kiriting> ");
+//        int S = scannerInt.nextInt();
+//
+//        double R = Math.sqrt(S/pi);
+//        double L = 2*pi*R;
+//        System.out.println("R=" + R + " S=" + S );
+
+//      16
+
+//        System.out.print("X1 son kiriting> ");
+//        int x1 = scannerInt.nextInt();
+//        System.out.print("X2 son kiriting> ");
+//        int x2 = scannerInt.nextInt();
+//
+//        System.out.println("|X2-X1|="+ Math.abs(x2-x1));
+
+//      17
+
+//        System.out.print("A nuqtani kiriting: ");
+//        int A = scannerInt.nextInt();
+//
+//        System.out.print("B nuqtani kiriting: ");
+//        int B = scannerInt.nextInt();
+//
+//        System.out.print("C nuqtani kiriting: ");
+//        int C = scannerInt.nextInt();
+//
+//        int AC = Math.abs(C - A);
+//        int BC = Math.abs(C - B);
+//
+//        int sum = AC + BC;
+//
+//        System.out.println("AC kesma uzunligi: " + AC);
+//        System.out.println("BC kesma uzunligi: " + BC);
+//        System.out.println("AC + BC = " + sum);
 
 
-        System.out.println("hello world");
+//    18
+
+
+//        System.out.print("A nuqtani kiriting: ");
+//        int A = scannerInt.nextInt();
+//
+//        System.out.print("B nuqtani kiriting: ");
+//        int B = scannerInt.nextInt();
+//
+//        System.out.print("C nuqtani kiriting: ");
+//        int C = scannerInt.nextInt();
+//
+//        if ((C > A && C < B) || (C > B && C < A)) {
+//            int AC = Math.abs(C - A);
+//            int BC = Math.abs(C - B);
+//
+//            int product = AC * BC;
+//
+//            System.out.println("AC kesma uzunligi: " + AC);
+//            System.out.println("BC kesma uzunligi: " + BC);
+//            System.out.println("Ko‘paytma (AC * BC): " + product);
+//        } else {
+//            System.out.println("Xatolik: C nuqta A va B orasida emas!");
+//        }
+
+
+//        19
+
+
+//        System.out.print("x1 ni kiriting: ");
+//        int x1 = scannerInt.nextInt();
+//
+//        System.out.print("y1 ni kiriting: ");
+//        int y1 = scannerInt.nextInt();
+//
+//        System.out.print("x2 ni kiriting: ");
+//        int x2 = scannerInt.nextInt();
+//
+//        System.out.print("y2 ni kiriting: ");
+//        int y2 = scannerInt.nextInt();
+//        int length = Math.abs(x2 - x1);
+//        int width = Math.abs(y2 - y1);
+//
+//        int perimeter = 2 * (length + width);
+//        int area = length * width;
+//
+//        System.out.println("Perimetri: " + perimeter);
+//        System.out.println("Yuzasi: " + area);
+
+//        20
+//
+//        System.out.print("x1: ");
+//        double x1 = scannerInt.nextDouble();
+//        System.out.print("y1: ");
+//        double y1 = scannerInt.nextDouble();
+//        System.out.print("x2: ");
+//        double x2 = scannerInt.nextDouble();
+//        System.out.print("y2: ");
+//        double y2 = scannerInt.nextDouble();
+//        double result = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+//        System.out.println(result);
+
+//        21
+
+//        System.out.print("x1: ");
+//        double x1 = scannerInt.nextDouble();
+//        System.out.print("y1: ");
+//        double y1 = scannerInt.nextDouble();
+//        System.out.print("x2: ");
+//        double x2 = scannerInt.nextDouble();
+//        System.out.print("y2: ");
+//        double y2 = scannerInt.nextDouble();
+//        System.out.print("x3: ");
+//        double x3 = scannerInt.nextDouble();
+//        System.out.print("y3: ");
+//        double y3 = scannerInt.nextDouble();
+//
+//        double a = Math.sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1)); // AB
+//        double b = Math.sqrt((x3 - x2)*(x3 - x2) + (y3 - y2)*(y3 - y2)); // BC
+//        double c = Math.sqrt((x1 - x3)*(x1 - x3) + (y1 - y3)*(y1 - y3)); // CA
+//
+//        double p = (a + b + c) / 2;
+//        double S = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+//
+//        System.out.println("Tomonlar uzunligi: a = " + a + ", b = " + b + ", c = " + c);
+//        System.out.println("Perimetri = " + (a + b + c));
+//        System.out.println("Yuzasi = " + S);
+
+
+//        22
+//        System.out.print("A Sonni kiriting: ");
+//        int A = scannerInt.nextInt();
+//
+//        System.out.print("B Sonni kiriting: ");
+//        int B = scannerInt.nextInt();
+//
+//        int temp = A;
+//        A = B;
+//        B = temp;
+//
+//        System.out.println("Qiymatlar almashtirildi:");
+//        System.out.println("A = " + A);
+//        System.out.println("B = " + B);
+
+//        23
+
+
 
     }
 }
